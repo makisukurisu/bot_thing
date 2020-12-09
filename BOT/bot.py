535bad033db08b9ed8d0baa2327a36a260a69464
makisukurisu <667strets@gmail.com>
2020-12-09 12:15:02 +0200

    Немного доработана обработка, добавлен класс для работы с пользователями (нужно написать алтернейт ветки для основного и прописать ветки других отзывов)

--- BOT/bot.py
+++ BOT/bot.py
@@ -3,14 +3,54 @@ import sqlite3
 from telebot import types
 
 bot = telebot.TeleBot('1447763558:AAHAnuDaqHbDLyvEruZSxSre408DBOB_7vU')
-db = sqlite3.connect('cl_db.db', check_same_thread=False)
+db = sqlite3.connect('db.db', check_same_thread=False)
 c = db.cursor()
 
 lst_GNB = ['Отлично', 'Хорошо', 'Плохо']
 
 chats = {'Neg': 'ID', 'Pos': 'ID', 'Neu': 'ID'}
 
+class U_TH():
+
+	def __init__(self, t_id, from_):
+		self.id = t_id
+		self.from_ = from_
+		self.answers = []
+	def new_ans(self, answ):
+		self.answers.append(answ)
+	def new_msg(self, msg):
+		self.message = msg
+	def new_numb(self, numb):
+		self.numb = numb
+	def get_all(self):
+		all = []
+		try:
+			all.append(self.id, self.from_, self.answers, self.message, self.numb)
+		except:
+			all = [self.id, self.from_, self.answers, self.message]
+		return all
+
+
+us_answers = []
 ###Misc###
+
+def append_to_us(id, info, fun = 'ans'):
+
+	for x in us_answers:
+		if x.id == id:
+			if fun == 'ans':
+				x.new_ans(info)
+			elif fun == 'msg':
+				x.new_msg(info)
+			elif fun == 'numb':
+				x.new_numb(info)
+
+
+def get_us(id):
+
+	for x in us_answers:
+		if x.id == id:
+			return x
 
 def get_GNB_markup():
 	''' Good, neutral, bad markup '''
@@ -106,6 +146,8 @@ def pult_msg(message):
 		start_msg(message)
 		return
 	if message.text in lst_GNB:
+		us_answers.append(U_TH(message.from_user.id, ''))
+		append_to_us(message.from_user.id, message.text)
 		msg = bot.send_message(message.chat.id, 'Оцените качество обслуживания операторов пульта', reply_markup = get_GNB_markup())
 		bot.register_next_step_handler(msg, gbr_msg)
 	else:
@@ -118,6 +160,7 @@ def gbr_msg(message):
 		start_msg(message)
 		return
 	if message.text in lst_GNB:
+		append_to_us(message.from_user.id, message.text)
 		msg = bot.send_message(message.chat.id, 'Оцените качество обслуживания службы ГБР', reply_markup = get_GNB_markup())
 		bot.register_next_step_handler(msg, tech_msg)
 	else:
@@ -130,21 +173,76 @@ def tech_msg(message):
 		start_msg(message)
 		return
 	if message.text in lst_GNB:
+		append_to_us(message.from_user.id, message.text)
 		msg = bot.send_message(message.chat.id, 'Оцените качество  работы технической группы', reply_markup = get_GNB_markup())
 		bot.register_next_step_handler(message, concl_msg)
 	else:
 		non_req_GNB(message, tech_msg)
 
-def tech_msg(message):
+def concl_msg(message):
+	
+	if message.text.find('/start') >= 0:
+		start_msg(message)
+		return
+	if message.text in lst_GNB:
+		append_to_us(message.from_user.id, message.text)
+		info = get_us(message.from_user.id)
+		if 'Плохо' in info.answers:
+			markup = types.ReplyKeyboardMarkup()
+			markup.add(types.KeyboardButton('Да'))
+			markup.add(types.KeyboardButton('Нет'))
+			msg = bot.send_message(message.chat.id, 'Вам что-то не понравилось, нам комментарий?', reply_markup = markup)
+			bot.register_next_step_handler(msg, get_msg_about)
+		else:
+			msg = bot.send_message(message.chat.id, 'Велп')
+	else:
+		non_req_GNB(message, tech_msg)
 
-	###Вывод с предложением написать сообщение###
-	None
+def get_msg_about(message):
 
-###def get_msg_about
+	if message.text in ['Да', 'Нет']:
 
-def get_nymb_subm(message):
+		if message.text == 'Да':
+			markup = types.ForceReply()
+			msg = bot.send_message(message.chat.id, 'Ваш отзыв тут же попадёт к нашим менеджерам!', reply_markup = markup)
+			bot.register_for_reply(msg, get_numb_subm)
+		else:
+			get_numb_subm(message)
+	else:
+		non_req_GNB(message, get_msg_about)
+def get_numb_subm(message):
 
-	###Функция для запроса номера###
-	None
+	msg_text = ''
+	if message.text == 'Нет':
+
+		msg_text = 'Сожалеем что наш сервис вас не устроил.'
+	elif message.text.find('/start') >= 0:
+		start_msg(message)
+		return
+	else:
+		append_to_us(message.from_user.id, message.text, 'msg')
+		if msg_text == '':
+			msg_text = 'Спасибо за ваш отзыв, теперь - финальный этап. Поделитесь с нами вашим номером телефона чтобы мы могли вам ответить и помочь с проблемами.'
+		markup = types.ReplyKeyboardMarkup()
+		markup.add(types.KeyboardButton('Передать номер телефона', True))
+		markup.add(types.KeyboardButton('Не предавать'))
+		msg = bot.send_message(message.chat.id, msg_text, reply_markup = markup)
+		bot.register_next_step_handler(msg, final_step)
+
+def final_step(message):
+
+	if type(message.contact) != type(None):
+		append_to_us(message.from_user.id, message.contact.phone_number, 'msg')
+		all = get_us(message.from_user.id).get_all()
+		bot.send_message(message.chat.id, 'Спасибо за ваш отзыв, будем расти вместе с вами!\n\n{}'.format(all))
+	elif message.text == 'Не передавать':
+		bot.send_message(message.chat.id, 'Нам будет труднее вас идентифицировать, но мы что-то придумаем')
+	elif message.text.find('/start') >= 0:
+		start_msg(message)
+		return
+	else:
+		non_req_GNB(message, final_step)
+
+
 
 bot.polling()
